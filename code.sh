08570696dd9869bebb8dc0e2b451483072f6da8d
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2023-01-24 12:34:05 +0300

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
